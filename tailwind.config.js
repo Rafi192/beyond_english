9662f8e00bb67f859dba2f314201092b78d4e061
Rafi192 <rafihasanmmsc@gmail.com>
2025-09-13 20:01:27 +0600

    Commit of beyond english initial codes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1E40AF',
+        customGreen: '#10B981',
+        
+      },
+    },
+  },
+  plugins: [],
+};
