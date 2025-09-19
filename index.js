f6e879feea57edea647b4365e07f776dc65f4798
Rafi192 <rafihasanmmsc@gmail.com>
2025-09-19 10:52:02 +0600

    Commit of adding words

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("i am jS")
+// console.log("i am jS")
 
 const url = "https://openapi.programming-hero.com/api/levels/all"
 
@@ -14,17 +14,60 @@ const loadLessons = ()=>{
     })
 }
 
+const loadLevelWord =(id)=>{
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    // console.log(url)
+    fetch(url)
+    .then((response)=>{
+        return response.json();
+    })
+    .then(data=>{
+        displayLevelWord(data.data);
+        
+    })
+}
+
+const displayLevelWord = (level)=>{
+    // get the container
+    const levelContainer = document.getElementById('word-container')
+    levelContainer.innerHTML = "";
+
+    level.forEach(word => {
+        const wordCard = document.createElement('div');
+        wordCard.innerHTML = `
+         <div id="card-container" class="bg-white text-center py-3 px-2">
+        <h2>${word.word}</h2>
+        <p>${word.meaning}</p>
+        <p>${word.pronunciation}</p>
+
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+        
+        `
+        levelContainer.append(wordCard)
+
+        
+    });
+
+    
+}
+
 const displayLesson = (lessons)=>{
     // 1.get the container
     const levelDiv = document.getElementById('level-container')
     levelDiv.innerHTML ="";
+
 
     // 2. get into every data
     for(let lesson of lessons){
          // 3. create element
          const btnDiv = document.createElement('div');
          btnDiv.innerHTML = `
-         <button class="btn btn-outline btn-primary">
+         <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
          
          <i class="fa-solid fa-book-open"></i>
          Lesson - ${lesson.level_no}
@@ -48,5 +91,8 @@ const displayLesson = (lessons)=>{
 
 }
 
+
+
 loadLessons()
 // loadLessons()
+loadLevelWord();
